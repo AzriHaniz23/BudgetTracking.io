04fbafec15e770e8cc80128ba2039cb3f585e30f
Azri Haniz <127809107+AzriHaniz23@users.noreply.github.com>
2023-08-10 14:08:33 +0800

    Update 2:08 pm
    Update 2:08 pm

--- resources/views/features.blade.php
+++ resources/views/features.blade.php
@@ -4,34 +4,6 @@
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg text-center">
             <h1 class="text-3xl font-semibold">What We Offer</h1>
-			
-			<div id="bank_2"  ></div>
-
-			<div id="calendar_2"  ></div>
-			
-			<div id="pngwing_13"  ></div>
-			
-			<div id="__pngtree_vector_transaction_icon_4101215__1__2"  ></div>
-			
-			<div id="bank_financing" >
-				Bank Financing
-			</div>
-			
-			<div id="what_we_offer" >
-				What We offer
-			</div>
-			
-			<div id="daily_budgeting" >
-				Daily Budgeting
-			</div>
-			
-			<div id="track_expenses" >
-				Track Expenses
-			</div>
-			
-			<div id="previous_transaction" >
-				Previous Transaction
-			</div>
 		</div>
     </div>
 @endsection
